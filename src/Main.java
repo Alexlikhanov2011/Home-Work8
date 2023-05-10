e2cc0623f29fed5fcd8d92f5f15ada20eb1c35d8
Alexlikhanov2011 <Likhanov-2011@yandex.ru>
2023-05-10 23:08:27 +0300

    начало дз

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+      task1();
+
+
+
+
+    }
+    public static void task1(){
+        int[] number = new int[3];
+        number [0] = 1;
+        number [1] = 2;
+        number [2] = 3;
+
+        double[] number1= {1.57, 7.654, 9.986};
+
+        int [] number3 = {'a','b','c', 'd'};
+    }
+}
